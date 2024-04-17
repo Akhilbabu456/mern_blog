054185c196130b9f0cedeab3b7123cce1e76145e
Akhilbabu456 <akhilbeliever001@gmail.com>
2024-04-17 07:32:17 +0530

    updates

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -12,9 +12,9 @@ export default function FooterCom() {
               className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
             >
               <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-                Akhil's
+                InkWell
               </span>
-              Blog
+              Chronicles
             </Link>
           </div>
           <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
@@ -33,7 +33,7 @@ export default function FooterCom() {
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  Akhil's Blog
+                  InkWell Chronicles
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
@@ -63,14 +63,14 @@ export default function FooterCom() {
         <div className='w-full sm:flex sm:items-center sm:justify-between'>
           <Footer.Copyright
             href='#'
-            by="Akhil's blog"
+            by="InkWell Chronicles"
             year={new Date().getFullYear()}
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
             <Footer.Icon href='#' icon={BsFacebook}/>
             <Footer.Icon href='#' icon={BsInstagram}/>
             <Footer.Icon href='#' icon={BsTwitter}/>
-            <Footer.Icon href='https://github.com/Akhilghavidel' icon={BsGithub}/>
+            <Footer.Icon href='https://github.com/Akhilbabu456' icon={BsGithub}/>
             <Footer.Icon href='#' icon={BsDribbble}/>
 
           </div>
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,20 +4,20 @@ export default function About() {
       <div className='max-w-2xl mx-auto p-3 text-center'>
         <div>
           <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Sahand' Blog
+            About InkWell' Chronicles
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
-              Welcome to Sahand's Blog! This blog was created by Sahand Ghavidel
+              Welcome to InkWell' Chronicles! This blog was created by Akhil Babu S
               as a personal project to share his thoughts and ideas with the
-              world. Sahand is a passionate developer who loves to write about
+              world. Akhil is a passionate developer who loves to write about
               technology, coding, and everything in between.
             </p>
 
             <p>
               On this blog, you'll find weekly articles and tutorials on topics
               such as web development, software engineering, and programming
-              languages. Sahand is always learning and exploring new
+              languages. Akhil is always learning and exploring new
               technologies, so be sure to check back often for new content!
             </p>
 
